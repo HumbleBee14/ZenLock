e8000e0d1d912a724c62e4fd127dddc68dc77a23
Dinesh Yadav <13635627+HumbleBee14@users.noreply.github.com>
2025-09-12 21:54:18 -0400

    v1.7.0 - Enabled 4 Whitelist Apps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.grepguru.zenlock"
         minSdk = 28
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.6.1"
+        versionCode = 12
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
