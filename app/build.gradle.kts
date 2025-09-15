ca8e800b227423101388628459abf1c4dcb706b7
Dinesh Yadav <13635627+HumbleBee14@users.noreply.github.com>
2025-09-14 23:33:31 -0400

    v1.7.1-  Build 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.grepguru.zenlock"
         minSdk = 28
         targetSdk = 35
-        versionCode = 13
+        versionCode = 14
         versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
