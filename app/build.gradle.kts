a9e3414cebfd77d678e847fc7b113801eeb33f48
Dinesh Yadav <13635627+HumbleBee14@users.noreply.github.com>
2026-03-20 02:04:12 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.grepguru.zenlock"
         minSdk = 28
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.8.1"
+        versionCode = 26
+        versionName = "1.8.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     // major.minor.patch
